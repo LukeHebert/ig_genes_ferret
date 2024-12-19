e9e5ba5deffdaab4b7f2f904bf658a2248ac947e
Luke Hebert <luke.s.hebert@utexas.edu>
2024-12-19 10:01:48 -0600

    Delete constant_gene_transcript_analyis directory
    mis-spelled directory name; made a new one already

--- constant_gene_transcript_analyis/blast_IGLCs.sh
+++
@@ -1,7 +0,0 @@
-/stor/work/Georgiou/Sharing_Folder/BLASTplus_2.15.0/ncbi-blast-2.15.0+/bin/blastn \
--query lambda/2_blast/F2_IGLC_merged.fasta \
--task megablast \
--db lambda/2_blast/IMGT_lambda_Cexons_db/IMGT_lambda_Cexons_fixed.fasta \
--out lambda/2_blast/F2_IGLC_merged.tsv \
--outfmt "6 qseqid qlen sseqid slen qstart qend sstart send qseq bitscore evalue length" \
--num_threads 40
